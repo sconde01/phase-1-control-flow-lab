13883c3ff6ee0710f50283742bdb8c167c8f93bc
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-06-10 15:33:21 -0700

    commit tree 0f06a16c2a63e22665033da9b455b0e25236e360

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if(someNumber < 400) {
+    result = 'This one is on me!'
+  }
+  else if(someNumber > 2000 && someNumber <= 2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if(someNumber >= 2500){
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
